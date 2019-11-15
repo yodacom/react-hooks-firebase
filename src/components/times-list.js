c39413397d7850e094849305ac07eabdaf71ac3d
Jeremy Black <jjb@Jeremys-MacBook-Pro.local>
2019-11-15 16:47:44 -0700

    added sorting

--- src/components/times-list.js
+++ src/components/times-list.js
@@ -1,59 +1,67 @@
 import React, { useState, useEffect } from "react";
 import firebase from "../firebase";
 
-function useTimes() {
-  const [times, setTimes] = useState([])
+const SORT_OPTIONS = {
+  'TIME_ASC': { column: 'time_seconds', direction: 'asc' },
+  'TIME_DESC': { column: 'time_seconds', direction: 'desc' },
+
+  'TITLE_ASC': { column: 'title', direction: 'asc' },
+  'TITLE_DESC': { column: 'title', direction: 'desc' }
+}
+
+function useTimes(sortBy = "TIMES_ASC") {
+  const [times, setTimes] = useState([]);
 
   useEffect(() => {
-    // todo: we need an unsubscribe callback not for youtube subscribers
+    // we need an unsubscribe callback to turn off socket when done so we name this const to be available to return at the end as an unsubscribe when finished.
     const unsubscribe = firebase
       .firestore()
-      .collection('times')
-      .onSnapshot((snapshot) => {
-        const newTimes = snapshot.docs.map((doc) => ({
+      .collection("times")
+      .orderBy(SORT_OPTIONS[sortBy].column, SORT_OPTIONS[sortBy].direction)
+      .onSnapshot(snapshot => {
+        const newTimes = snapshot.docs.map(doc => ({
           id: doc.id,
           ...doc.data()
-        }))
+        }));
 
-        setTimes(newTimes)
-      })
-    return () => unsubscribe()
-  }, [])
+        setTimes(newTimes);
+      });
+    return () => unsubscribe();
+  }, [sortBy]);
 
-  return times
+  return times;
 }
 
 const TimesList = () => {
-  const times = useTimes()
+  const [sortBy, setSortBy] = useState('TIME_ASC')
+  const times = useTimes(sortBy);
 
   return (
     <div>
       <h2>Times List</h2>
       <div>
-        <label>Sort by</label>{''}
-        <select>
-        <option>Time (fastest first)</option>
-        <option>Time (slowest first) </option>
-        <option disabled >------</option>
-        <option>Title(a-z)</option>
-        <option>Title(z-a)</option>
-
+        <label>Sort by</label>
+        {""}
+        <select value={sortBy} onChange={e => setSortBy(e.currentTarget.value)}>
+          <option value="TIME_ASC">Time (fastest first)</option>
+          <option value="TIME_DESC">Time (slowest first) </option>
+          <option disabled>------</option>
+          <option value="TITLE_ASC">Title(a-z)</option>
+          <option value="TITLE_DESC">Title(z-a)</option>
         </select>
       </div>
       <ol>
-        {times.map((time) =>
-        <li key={time.id}>
-          <div className="time-entry">
-            {time.title}
+        {times.map(time => (
+          <li key={time.id}>
+            <div className="time-entry">
+              {time.title}
               <code className="time"> {time.time_seconds} seconds </code>
-          </div>
-        </li>
-
-    )}
-
+            </div>
+          </li>
+        ))}
       </ol>
     </div>
-  )
-}
+  );
+};
 
 export default TimesList;
